6fb5e44fc534fc0a123fae731d28a169a533d1db
lunarxlark <lunarxlark@gmail.com>
2018-09-19 00:11:58 +0900

    session3.25 display post title in admin site

--- myblogapp/posts/models.py
+++ myblogapp/posts/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title     = models.CharField(max_length=100)
     published = models.DateTimeField()
-    image = models.ImageField(upload_to='media/')
-    body = models.TextField()
+    image     = models.ImageField(upload_to='media/')
+    body      = models.TextField()
+
+    def __str__(self):
+        return self.title
